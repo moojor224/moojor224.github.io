fd8e6a51a3932c2386612fa868c6b26babc880ef
moojor224 <moojor224@gmail.com>
2023-09-23 01:47:28 -0500

    started adding global asset loading

--- jmoore143/js/global.js
+++ jmoore143/js/global.js
@@ -1,13 +1,41 @@
-function loadNav() {
-    fetch("/jmoore143/helpers/nav.html").then(r => {
+function loadScripts() {
+    if (!window.createElement) {
+        var scr = document.createElement("script");
+        scr.src = "/jmoore143/js/jstools.js";
+        document.head.append(scr);
+    }
+    if(!["0px", "0"].includes(window.getComputedStyle(document.body).margin)){
+        var lnk = document.createElement("link");
+        lnk.rel = "stylesheet";
+        lnk.href = "/jmoore143/css/global.css";
+        document.head.append(lnk);
+    }
+    loadNav();
+}
+async function loadNav() {
+    console.debug("getting nav");
+    await fetch("/jmoore143/helpers/nav.html").then(r => {
         r.text().then(t => {
-            var nav = document.body.querySelector("nav");
+            var nav;
+            while (true) {
+                if (!(nav = document.body.querySelector("nav"))) {
+                    if (!(nav = document.querySelector("header"))) {
+                        document.body.prepend(
+                            createElement("header").add(nav = createElement("nav"))
+                        );
+                        break;
+                    }
+                    nav.add(nav = createElement("nav"));
+                    break;
+                }
+                break;
+            }
             nav.innerHTML = t;
 
             var scr = document.querySelector("nav script");
             var nw = document.createElement("script");
             nw.innerHTML = scr.innerHTML;
-            scr.onload = function (){
+            scr.onload = function () {
 
             }
             scr.async = true;
@@ -15,4 +43,4 @@ function loadNav() {
         });
     });
 }
-window.onload = loadNav;
+window.onload = loadScripts;
