ed5d8b228c82de0f21dec6317c63ce48e07e9f7a
moojor224 <moojor224@gmail.com>
2023-11-16 17:54:13 -0600

    3d render

--- fall2023/jmoore143/games/3d renderer/app.js
+++ fall2023/jmoore143/games/3d renderer/app.js
@@ -12,61 +12,37 @@ Number.prototype.wrapRange = function (min, max) {
     }
     return val;
 }
-/*
-let angle = 0, dx = 0, dy = 0, keys = {}, lookamount = 5, moveamount = 5, angleadjust = 270;
+
+let keys = {}, angleadjust = 270;
 // w moves:
 // 0: right
-// 90: down
+// 90: downI
 // 180: left
 // 270: up
+let ly = 0, lx = 0, lookamount = 2, dx = 0, dz = 0, moveamount = 10;
 function moveForward(amount) {
-    let x = Math.cos((angle + angleadjust) * Math.PI / 180) * amount;
-    let y = Math.sin((angle + angleadjust) * Math.PI / 180) * amount;
+    let x = Math.cos((lx + angleadjust) * Math.PI / 180) * amount;
+    let y = Math.sin((lx + angleadjust) * Math.PI / 180) * amount;
     dx += x;
-    dy += y;
+    dz += y;
     document.querySelector(':root').style.setProperty('--dx', dx + 'px');
-    document.querySelector(':root').style.setProperty('--dy', dy + 'px');
+    document.querySelector(':root').style.setProperty('--dz', dz + 'px');
 }
 function moveLateral(amount) {
-    let x = Math.cos((angle + 90 + angleadjust) * Math.PI / 180) * amount;
-    let y = Math.sin((angle + 90 + angleadjust) * Math.PI / 180) * amount;
+    let x = Math.cos((lx + 90 + angleadjust) * Math.PI / 180) * amount;
+    let y = Math.sin((lx + 90 + angleadjust) * Math.PI / 180) * amount;
     dx += x;
-    dy += y;
+    dz += y;
     document.querySelector(':root').style.setProperty('--dx', dx + 'px');
-    document.querySelector(':root').style.setProperty('--dy', dy + 'px');
+    document.querySelector(':root').style.setProperty('--dz', dz + 'px');
 }
-document.body.addEventListener("keydown", function (event) {
-    event.preventDefault();
-    keys[event.key.toLowerCase()] = true;
-});
-document.body.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    keys[event.key.toLowerCase()] = false;
-});
 window.setInterval(function () {
-    if (keys["arrowleft"]) {
-        angle -= lookamount;
-    }
-    if (keys["arrowright"]) {
-        angle += lookamount;
-    }
-    document.querySelector(':root').style.setProperty('--angle', angle + 'deg');
+    document.querySelector(':root').style.setProperty('--ly', ly + 'deg');
 
-    if (keys["w"]) {
-        moveForward(moveamount * (keys["shift"] ? 2 : 1));
-    }
-    if (keys["s"]) {
-        moveForward(-moveamount * (keys["shift"] ? 2 : 1));
-    }
-    if (keys["d"]) {
-        moveLateral(moveamount * (keys["shift"] ? 2 : 1));
-    }
-    if (keys["a"]) {
-        moveLateral(-moveamount * (keys["shift"] ? 2 : 1));
-    }
+    
 }, 1000 / 60);
-*/
-let keys = {};
+
+// let keys = {};
 document.body.addEventListener("keydown", function (event) {
     event.preventDefault();
     keys[event.key.toLowerCase()] = true;
@@ -75,7 +51,6 @@ document.body.addEventListener("keyup", function (event) {
     event.preventDefault();
     keys[event.key.toLowerCase()] = false;
 });
-let ly = 0, lx = 0, lookamount = 2, dx = 0, dz = 0, moveamount = 10;
 window.setInterval(function () {
     if (keys["arrowup"]) {
         ly += lookamount;
@@ -95,17 +70,15 @@ window.setInterval(function () {
     document.querySelector(':root').style.setProperty('--ry', lx + 'deg');
 
     if (keys["w"]) {
-        dz += moveamount * (keys["shift"] ? 2 : 1);
-    }
-    if (keys["a"]) {
-        dx += moveamount * (keys["shift"] ? 2 : 1);
+        moveForward(-moveamount * (keys["shift"] ? 2 : 1));
     }
     if (keys["s"]) {
-        dz -= moveamount * (keys["shift"] ? 2 : 1);
+        moveForward(moveamount * (keys["shift"] ? 2 : 1));
     }
     if (keys["d"]) {
-        dx -= moveamount * (keys["shift"] ? 2 : 1);
+        moveLateral(-moveamount * (keys["shift"] ? 2 : 1));
     }
-    document.querySelector(':root').style.setProperty('--dz', dz + 'px');
-    document.querySelector(':root').style.setProperty('--dx', dx + 'px');
+    if (keys["a"]) {
+        moveLateral(moveamount * (keys["shift"] ? 2 : 1));
+    }
 }, 1000 / 60);
