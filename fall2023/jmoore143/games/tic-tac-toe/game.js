69bd10e37d3bcbe91e5ba2bd80df7ab3fa3dbfa5
moojor224 <moojor224@gmail.com>
2023-11-20 20:42:57 -0600

    tic tac toe

--- fall2023/jmoore143/games/tic-tac-toe/game.js
+++ fall2023/jmoore143/games/tic-tac-toe/game.js
@@ -6,17 +6,25 @@ class Game {
         this.#makeBoard(size);
         let game = createElement("game");
         this.scoreboard = createElement("scoreboard").add(
-			createElement("row").add(createElement("cell").add(
+            createElement("row").add(createElement("cell").add(
                 createElement("div").add(createElement("i", {
                     classList: "fa-solid fa-x"
                 }))
-            ),createElement("cell").add(
+            ), createElement("cell").add(
                 createElement("div").add(createElement("i", {
                     classList: "fa-solid fa-o"
                 }))
             )),
-			createElement("row").add(createElement("cell"),createElement("cell"))
-		);
+            createElement("row").add(createElement("cell", {
+                dataset: {
+                    player: "x"
+                }
+            }), createElement("cell", {
+                dataset: {
+                    player: "o"
+                }
+            }))
+        );
         game.add(this.scoreboard);
         game.add(this.board);
         document.querySelector("main").add(game);
@@ -31,30 +39,85 @@ class Game {
         this.board = createElement("board").add(
             ...new Array(size).fill("").map(r => createElement("row").add(
                 ...new Array(size).fill("").map(c => createElement("cell").add(
-                    createElement("div").add(createElement("i"))
+                    createElement("div", {
+                        onclick: (e) => this.play(e.target)
+                    }).add(createElement("i"))
                 ))
             ))
         );
     }
     check() {
+        console.log("checking");
         let win = false;
-        for(var i in this.groups){
-            let e = this.groups[i].map(e=>e.dataset.marker);
-            if([...new Set(e)].length == 1 && ["x", "o"].includes(e[0])){
-                return this.groups[i];
+        let winningGroup;
+        let full = true;
+        for (var i in this.groups) {
+            let e = this.groups[i].map(e => e.dataset.marker);
+            if(e){
+                full = false;
+            }
+            if ([...new Set(e)].length == 1 && ["x", "o"].includes(e[0])) {
+                winningGroup = this.groups[i];
+                break;
             }
         }
+        let game = this;
+        if(full){
+            window.setTimeout(function(){
+                alert("tie!");
+                game.clear();
+                [game.order[0], game.order[1]] = [game.order[1], game.order[0]];
+                game.turn = false;
+            }, 100)
+        }
+        if (win) {
+            window.setTimeout(function () {
+                let winner = (winningGroup[0].dataset.marker);
+                game[winner + "_score"]++;
+                alert(winner + " wins");
+                setTimeout(function () {
+                    game.clear();
+                    game.score();
+                    [game.order[0], game.order[1]] = [game.order[1], game.order[0]];
+                    game.turn = false;
+                }, 1000);
+            }, 100);
+        }
     }
-    tally(number){
+    tally(number) {
         let arr = [..."abcde"];
-        let str = "";
-        while(number > 4){
+        let str = " ";
+        while (number > 4) {
             str += "e";
             number -= 5;
         }
-        if(number >0){
-            str += arr[number-1];
+        if (number > 0) {
+            str += arr[number - 1];
         }
         return str;
+    }
+    clear() {
+        this.groups.forEach(g => g.forEach(e => e.dataset.marker = ""));
+
+    }
+    score() {
+        this.scoreboard.querySelector("[data-player='x']").innerHTML = this.tally(this.x_score);
+        console.log(this.tally(this.x_score));
+        this.scoreboard.querySelector("[data-player='o']").innerHTML = this.tally(this.o_score);
+        console.log(this.tally(this.o_score));
+    }
+    turn = false;
+    order = ["x", "o"];
+    play(el) {
+        while (el.tagName.toLowerCase() !== "cell") {
+            el = el.parentElement;
+        }
+        if (el.dataset.marker) {
+            return;
+        }
+        el.dataset.marker = this.order[+this.turn];
+        this.turn = !this.turn;
+        this.check();
+
     }
 }
