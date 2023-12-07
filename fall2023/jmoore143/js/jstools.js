1d2a1336de891d68683e88c19f83defc9ce632ae
moojor224 <moojor224@gmail.com>
2023-12-07 17:53:24 -0600

    add extend function

--- fall2023/jmoore143/js/jstools.js
+++ fall2023/jmoore143/js/jstools.js
@@ -1,7 +1,7 @@
 // <!-- copyright Jordan Moore (jmoore143) -->
 
 function createElement(tag = "span", data = {}) {
-    tag = typeof(tag) === "string" ? document.createElement(tag) : tag;
+    tag = typeof (tag) === "string" ? document.createElement(tag) : tag;
     Object.keys(data).forEach(e => {
         if (typeof data[e] === "object") {
             createElement(tag[e] || (tag[e] = {}), data[e]);
@@ -12,7 +12,7 @@ function createElement(tag = "span", data = {}) {
     return tag;
 }
 
-window.Element.prototype.add = function(...args) {
+window.Element.prototype.add = function (...args) {
     args.forEach(elem => {
         this.append(elem);
     });
@@ -21,4 +21,19 @@ window.Element.prototype.add = function(...args) {
 
 function displaySecs(seconds) {
     return new Date(seconds * 1000).toISOString().slice(11, 19);
+}
+
+function extend(template, config) {
+    let temp = Object.assign({}, template);
+    Object.entries(config).forEach(function(entry) {
+        if (temp[entry[0]] == undefined) {
+            temp[entry[0]] = {};
+        }
+        if (entry[1].constructor.name == "Object") {
+            temp[entry[0]] = extend(temp[entry[0]], entry[1]);
+        } else {
+            temp[entry[0]] = config[entry[0]];
+        }
+    });
+    return temp;
 }
