0cf36a417990907756b6a7ddfd59862df3ad0cf8
moojor224 <moojor224@gmail.com>
2023-12-08 13:22:33 -0600

    fix stupid styling decision

--- fall2023/jmoore143/js/backhome.js
+++ fall2023/jmoore143/js/backhome.js
@@ -5,9 +5,6 @@ window.onload = function () {
     a.onclick = ()=>window.location="/fall2023/jmoore143";
     a.className = "home";
     a.innerHTML = "back home";
-    let style = document.createElement("style");
-    style.innerHTML = `a.home {cursor: pointer;color: #0000;background-color: #0000;position: absolute;top: 0px;left: 0px;padding: 5px;}
-a.home:visited {color: #0000;background-color: #0000;}a.home:hover {color: #fff;background-color: #000;}`;
-    document.head.append(style);
+    a.style = `cursor: pointer;color: #0000;background-color: #0000;position: absolute;top: 0px;left: 0px;padding: 5px;`;
     document.body.append(a);
 }
